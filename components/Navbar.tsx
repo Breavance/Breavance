434fbb5360a0d82f19defc52dc1b81c8eab32293
Bree2003 <breeale2003@gmail.com>
2024-12-04 21:00:09 -0300

    fix nabvar, made it responsive and change logo png

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,33 +1,59 @@
+"use client";
+import { Bars3Icon, XMarkIcon } from "@heroicons/react/16/solid";
 import Image from "next/image";
 import Link from "next/link";
+import { useState } from "react";
 
 export default function Navbar() {
+  const [open, setOpen] = useState(false);
+  const onClickHamburguer = () => setOpen(!open);
+
   return (
-    <div className="flex justify-between font-semibold text-white p-5">
+    <div className="flex items-center justify-between font-semibold text-white p-5">
       <div className="justify-items-start">
         <Link href={"/"}>
           <Image alt="Breavance" src={"/nombre.png"} width={200} height={200} />
         </Link>
       </div>
-      <div className="flex gap-7">
-        <Link
-          href={"/sobre-nosotros"}
-          className="hidden sm:block relative transition-transform duration-300 hover:scale-110 hover:text-shadow-white"
+
+      <div className="relative md:static">
+        <div
+          className="md:hidden relative md:static flex justify-end"
+          onClick={onClickHamburguer}
         >
-          Sobre Nosotros
-        </Link>
-        <Link
-          href={"/contactanos"}
-          className="hidden sm:block relative transition-transform duration-300 hover:scale-110 hover:text-shadow-white"
-        >
-          Contáctanos
-        </Link>
-        <Link
-          href={"/nuestros-proyectos"}
-          className="hidden sm:block relative transition-transform duration-300 hover:scale-110 hover:text-shadow-white"
-        >
-          Nuestros Proyectos
-        </Link>
+          {open ? (
+            <XMarkIcon className="text-white w-6" />
+          ) : (
+            <Bars3Icon className="text-white w-6" />
+          )}
+        </div>
+
+        <div className="absolute md:static right-[5px] top-[30px] md:right-0 md:top-0">
+          <div
+            className={`${
+              open ? "flex" : "hidden"
+            } gap-2 md:flex md:gap-7 flex-col md:flex-row text-right`}
+          >
+            <Link
+              href={"/sobre-nosotros"}
+              className="relative transition-transform duration-300 hover:scale-110 hover:text-shadow-white w-[145px]"
+            >
+              Sobre Nosotros
+            </Link>
+            <Link
+              href={"/contactanos"}
+              className="relative transition-transform duration-300 hover:scale-110 hover:text-shadow-white"
+            >
+              Contáctanos
+            </Link>
+            <Link
+              href={"/nuestros-proyectos"}
+              className="relative transition-transform duration-300 hover:scale-110 hover:text-shadow-white"
+            >
+              Nuestros Proyectos
+            </Link>
+          </div>
+        </div>
       </div>
     </div>
   );
