e00e9b7b8583e5db2adede1e6fac345051844c50
Bree2003 <breeale2003@gmail.com>
2025-05-26 21:12:15 -0400

    update footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,9 +1,7 @@
 export default function Footer() {
   return (
     <div className="bg-[#A0AAB2] text-center text-base px-5">
-      <span className="text-black">
-        Copyright © {new Date().getFullYear()} | Breavance
-      </span>
+      <span className="text-black">Copyright © 2025 | Breavance</span>
     </div>
   );
 }
